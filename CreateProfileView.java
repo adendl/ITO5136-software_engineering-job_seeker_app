c208ceeef8103e976d72399f00190892bf13f1ec
Heynus <ben.heynes@outlook.com>
2022-11-15 20:47:28 +0800

    Create Profile View added

---
+++ CreateProfileView.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+
+public class CreateProfileView {
+
+    private JPanel createUserPanel;
+    private JLabel pageTitleText;
+    private JTextField firstNameTextField;
+    private JPasswordField confirmPasswordField;
+    private JComboBox userTypeComboBox;
+    private JTextField lastNameTextField;
+    private JPasswordField passwordField;
+    private JTextField emailTextField;
+    private JLabel lastNameLabel;
+    private JButton createAccountButton;
+
+    public CreateProfileView() {
+        createAccountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
