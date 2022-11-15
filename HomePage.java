d320a76e697ccb04729844bbaedd97e167f0ad47
adel0025 <adel0025@student.monash.edu>
2022-11-15 20:54:20 +1100

    Added job seeker home view

---
+++ HomePage.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HomePage {
+    private JButton searchJobsButton;
+    private JPanel panel1;
+    private JButton viewApplicationsButton;
+    private JButton viewInterviewInvitationsButton;
+    private JButton editProfileButton;
+
+    public HomePage() {
+        searchJobsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewApplicationsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewInterviewInvitationsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        editProfileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
