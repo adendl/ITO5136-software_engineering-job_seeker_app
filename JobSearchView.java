1821accf40ea3803313d92018f55c5e250500aa3
Joel Paterno <joelpaterno1@gmail.com>
2022-11-17 08:08:22 +1100

    mock up of Views

---
+++ JobSearchView.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class JobSearchView {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JButton button1;
+    private JButton button2;
+}
