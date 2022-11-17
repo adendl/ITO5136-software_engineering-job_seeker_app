62eaaed0d1b45852e33bfd361406d70bf80e782d
Andrew Bright <abri0019@student.monash.edu>
2022-11-17 19:37:15 +1100

    wip navigation view/controler

---
+++ controller/NavigationController.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class NavigationController {
+    // TODO: set up the navigation interface and stack
+    // create navigation view
+    // implement functions for the view to call
+    // doBack(), doLogout()
+}
---
+++ view/NavigationBar.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.*;
+
+public class NavigationBar {
+    private JButton backButton;
+    private JButton logoutButton;
+
+
+    // TODO: hook up button actions to NavigationController methods
+    //private NavigationController navigationController;
+    //navigationController.doBack()
+    //navigationController.doLogout()
+}
