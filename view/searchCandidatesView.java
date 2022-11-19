b912e73c1f97f7fb0ae012601cd14ce635a0c5e7
adel0025 <adel0025@student.monash.edu>
2022-11-19 20:47:51 +1100

    add first 2 csvs

---
+++ view/searchCandidatesView.java
@@ -0,0 +1,53 @@
+package view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class searchCandidatesView extends JFrame{
+    private JPanel panelMain;
+    private JTextField enterKeywordsTextField;
+    private JButton searchButton;
+    private JComboBox locationBox;
+    private JComboBox comboBox2;
+    private JList list1;
+    private JButton selectCategoriesButton;
+    private JScrollPane scrollPane;
+
+
+    public searchCandidatesView() {
+        selectCategoriesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (scrollPane.isVisible())
+                {
+                    scrollPane.setVisible(false);
+                }
+                else
+                {
+                    scrollPane.setVisible(true);
+                }
+            }
+        });
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(list1.getSelectedValuesList().toString());
+            }
+        });
+    }
+
+    public static void main(String[] args)
+    {
+        searchCandidatesView s = new searchCandidatesView();
+        s.setContentPane(s.panelMain);
+        s.setSize(800, 600);
+        DefaultListModel dlm = new DefaultListModel<String>();
+        for (int x = 0; x < 1000; x++)
+        {
+            dlm.add(x, x);
+        }
+        s.list1.setModel(dlm);
+        s.setVisible(true);
+    }
+}
