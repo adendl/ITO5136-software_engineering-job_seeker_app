9499fe7f5524b10a8bf6ed39c22e632077776dcb
adel0025 <adel0025@student.monash.edu>
2022-11-19 22:00:14 +1100

    added manage categories view

---
+++ view/ManageCategoriesView.java
@@ -0,0 +1,54 @@
+package view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ManageCategoriesView extends JFrame {
+    private JPanel panelMain;
+    private JList list1;
+    private JTextField addBox;
+    private JButton addButton;
+    private JButton deleteButton;
+    private JLabel deleteText;
+    private JLabel addText;
+    private JTextField deleteBox;
+
+    private DefaultListModel<String> dlm = new DefaultListModel<String>();
+
+    public ManageCategoriesView() {
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(addBox.getText());
+                if (dlm.contains(addBox.getText()))
+                {
+                    System.out.println("NOT ALLOWED");
+                }
+                else
+                {
+                    dlm.addElement(addBox.getText());
+                    list1.setModel(dlm);
+                }
+            }
+        });
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (dlm.contains(deleteBox.getText()))
+                {
+                    dlm.remove(dlm.indexOf(deleteBox.getText()));
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args)
+    {
+        ManageCategoriesView m = new ManageCategoriesView();
+        m.setContentPane(m.panelMain);
+        m.setSize(800, 600);
+        m.setVisible(true);
+
+    }
+}
