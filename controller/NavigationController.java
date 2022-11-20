b59f19103cdde5e3ff852968c97cf7a8bf23816f
Andrew Bright <abri0019@student.monash.edu>
2022-11-20 14:57:09 +1100

    more wip on nav bar and controller

--- controller/NavigationController.java
+++ controller/NavigationController.java
@@ -1,8 +1,27 @@
 package controller;
 
+import javax.swing.*;
+import java.util.ArrayDeque;
+
 public class NavigationController {
-    // TODO: set up the navigation interface and stack
-    // create navigation view
-    // implement functions for the view to call
-    // doBack(), doLogout()
+    JPanel contentArea;
+    public NavigationController() {
+        // todo: create navigation view stack and set up anything that needs
+    }
+
+    public void doBack() {
+        JOptionPane.showMessageDialog(null, "logout clicked");
+    }
+
+    public void doLogout() {
+        JOptionPane.showMessageDialog(null, "logout clicked");
+    }
+
+    public JPanel getContentArea() {
+        return contentArea;
+    }
+
+    public void setContentArea(JPanel contentArea) {
+        this.contentArea = contentArea;
+    }
 }
--- view/NavigationBar.java
+++ view/NavigationBar.java
@@ -1,14 +1,29 @@
 package view;
 
+import controller.NavigationController;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class NavigationBar {
     private JButton backButton;
     private JButton logoutButton;
+    private JPanel mainPanel;
 
+    NavigationController controller;
 
-    // TODO: hook up button actions to NavigationController methods
-    //private NavigationController navigationController;
-    //navigationController.doBack()
-    //navigationController.doLogout()
+    public NavigationBar(NavigationController controller) {
+        this.controller = controller;
+        backButton.addActionListener((ActionEvent e) -> {
+            controller.doBack();
+        });
+        logoutButton.addActionListener((ActionEvent e) -> {
+            controller.doLogout();
+        });
+    }
+
+    public JComponent getUIComponent() {
+        return mainPanel;
+    }
 }
