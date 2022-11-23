f57d3560637ea38a56b74afbb561bf7a66578389
Joel Paterno <joelpaterno1@gmail.com>
2022-11-23 17:13:21 +1100

    fixed JobSearchController

--- controller/JobSearchController.java
+++ controller/JobSearchController.java
@@ -8,31 +8,15 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class JobSearchController {
+    public JobSearchController(NavigationController navigationController, SearchJobView searchJobView, JobDetailsView jobDetailsView, SearchResultsView searchResultsView){
 
-    //this needs to take all of the views and models the controller handles and the navigation controller
-    public JobSearchController(JobSearchView jobSearchView, JobDetailsView jobDetailsView, NavigationController navigationController){
-
-        jobSearchView.addSearchButtonActionListener(new ActionListener() {
+        searchJobView.addSearchJobButtonListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //get field attributes (these really need to be int)
-
-                //send to searchActionController
-
-                //pass the job details panel to the navigation controller
-                navigationController.setContentArea(jobDetailsView.getJobDetailsPanel());
+                //get all field attributes and search term and run through matching algorithm
+                navigationController.setContentArea(searchResultsView.getPanelMain());
             }
         });
-
-        jobSearchView.addSearchOnProfileButtonActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //get field attributes and send to searchActionController
-                //bring search results into frame
-                navigationController.setContentArea(jobDetailsView.getJobDetailsPanel());
-            }
-        });
-
     }
 
 
