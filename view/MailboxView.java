0bb8f45a037414506d36ae9e5fc2bfb308adc3d4
Andrew Bright <abri0019@student.monash.edu>
2022-11-24 19:55:16 +1100

    add navigation compatibility to MailboxView

--- view/MailboxView.java
+++ view/MailboxView.java
@@ -4,9 +4,9 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MailboxView extends JPanel{
+public class MailboxView implements UIView {
     private JTable table1;
-    private JPanel panelmain;
+    private JPanel panelMain;
     private JComboBox sortByBox;
     private JButton previousButton;
     private JButton nextButton;
@@ -24,5 +24,15 @@ public class MailboxView extends JPanel{
 
             }
         });
+    }
+
+    @Override
+    public JComponent getUIView() {
+        return panelMain;
+    }
+
+    public static void main(String[] args) {
+        MailboxView view = new MailboxView();
+        ViewHelper.showStandaloneFrame(view);
     }
 }
