006e8a0936ed908b06ecf25a067bda9bd353f22a
Andrew Bright <abri0019@student.monash.edu>
2022-11-30 00:30:12 +1100

    hook up invitations view

--- controller/HomeJobSeekerController.java
+++ controller/HomeJobSeekerController.java
@@ -9,15 +9,6 @@ public class HomeJobSeekerController {
     // TODO: we should probably have a User passed in here as well so we can pass it along as needed
     public HomeJobSeekerController(NavigationController navigationController) {
         this.navigationController = navigationController;
-        /*
-        //send to mailboxView
-        homePageJobSeekerView.addViewInvitationsButtonListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                navigationController.pushView(mailboxView);
-            }
-        });
-        */
     }
 
     public void showHub() {
@@ -36,12 +27,14 @@ public class HomeJobSeekerController {
     }
 
     public void showInvitations() {
-        // TODO: MailboxView but it's also used by the Recruiter controller which might complicate things
+        // this could probably use MailboxController/View instead?
+        InvitationsView invitationsView = new InvitationsView(this);
+        navigationController.pushView(invitationsView);
     }
 
     public void showEditProfile() {
         // TODO: we should have a legit user object already, instead of this placeholder
-        // this placeholder user just gives us something to fill the fields
+        // this placeholder user just gives us something to fill the form fields
         JobSeeker user = new JobSeeker();
         user.setFirstName("Test");
         user.setLastName("User");
