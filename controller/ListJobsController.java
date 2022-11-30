f8e9e6d3bafdcf0bf132362d74599e4f55b904f6
Joel Paterno <joelpaterno1@gmail.com>
2022-11-30 15:33:06 +1100

    fixed listAppliedJobsController

--- controller/ListJobsController.java
+++ controller/ListJobsController.java
@@ -1,8 +1,20 @@
 package controller;
 
 import view.JobListingsView;
+import model.User;
 
 public class ListJobsController {
-    NavigationController navigationController;
+    private NavigationController navigationController;
+
+    User user;
+
+    public ListJobsController(NavigationController navigationController, User user){
+        this.navigationController = navigationController;
+        this.user = user;
+    }
+
+    public void renderListedJobs(){
+        //TODO: render table of listed jobs for the user
+    }
 
 }
