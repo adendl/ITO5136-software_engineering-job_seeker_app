c82af1266e2aa3cd1e54c050ee9fee84982aaff4
Heynus <ben.heynes@outlook.com>
2022-12-01 18:38:58 +0800

    Added table rendering to MailboxView

--- view/MailboxView.java
+++ view/MailboxView.java
@@ -1,13 +1,18 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
 import controller.MailboxController;
-import model.Mailbox;
+import model.DBConnection;
+import model.Message;
+import model.TableModelCreator;
 
 public class MailboxView implements UIView {
-    private JTable table1;
+    private JTable tblMessages;
     private JPanel panelMain;
     private JComboBox sortByBox;
     private JButton previousButton;
@@ -17,6 +22,23 @@ public class MailboxView implements UIView {
 
     public MailboxView(MailboxController controller) {
         this.controller = controller;
+
+        ResultSet rs = DBConnection.queryDatabase(DBConnection.connectDb(), "select messageID, subject, sendDate, title, firstName, lastName, messageType, messageStatus\nfrom Message\nINNER JOIN Job on Message.jobId=Job.jobId\nInner Join JobSeeker on Message.senderUserId=JobSeeker.userId");
+        DefaultTableModel dft = TableModelCreator.resultSetToTableModel(rs);
+        ActionListener listener = e -> {
+            int row = tblMessages.convertRowIndexToModel(tblMessages.getEditingRow());
+            int col = tblMessages.getColumn("messageId").getModelIndex();
+            DBConnection.queryDatabase(DBConnection.connectDb(), "delete from Message where messageId=" + (Integer)tblMessages.getValueAt(row, col));
+            ((DefaultTableModel)tblMessages.getModel()).removeRow(row);
+        };
+
+        TableModelCreator.addActionColumn(dft, "Delete", "Delete", listener);
+        tblMessages.setModel(dft);
+        tblMessages.setColumnSelectionAllowed(false);
+        tblMessages.setRowSelectionAllowed(false);
+        JTableButtonRenderer renderer = new JTableButtonRenderer();
+        tblMessages.getColumn(tblMessages.getColumnName(tblMessages.getColumnCount() - 1)).setCellRenderer(renderer);
+        tblMessages.getColumn(tblMessages.getColumnName(tblMessages.getColumnCount() - 1)).setCellEditor(renderer);
     }
 
     public JPanel getPanelMain() {
