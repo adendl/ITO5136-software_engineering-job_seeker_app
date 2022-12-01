66f6798561e8efd82ebb78d660ad6fbf5b72812e
adel0025 <adel0025@student.monash.edu>
2022-12-01 19:33:56 +1100

    changed it to 2 arraylist of type Keyword

--- controller/MatchingScore.java
+++ controller/MatchingScore.java
@@ -1,34 +1,41 @@
 package controller;
 
+import model.Keyword;
+
 import java.util.ArrayList;
 
 public class MatchingScore {
 
-    public static double calculateMatchingScore(ArrayList<String> keywordIds1, ArrayList<String> keywordIds2)
-    {
+    public static double calculateMatchingScore(ArrayList<Keyword> keywordIds1, ArrayList<Keyword> keywordIds2)
+    {//takes two keyword lists (in string form)
         double matchingScore = 0;
         for (int i = 0; i < keywordIds1.size(); i++)
-        {
+        { // for each item in  list1, it checks if this exists in list2
             System.out.println("time is " + i);
             if (keywordIds2.contains(keywordIds1.get(i)))
             {
-                matchingScore++;
+                matchingScore++; //if yes, the matching score is incremented
                 System.out.println(matchingScore);
             }
         }
-        return matchingScore / keywordIds1.size();
+        return matchingScore / keywordIds1.size(); //finally, the matching score is normalised against the size of the list
     }
 
     public static void main(String[] args)
     {
-        ArrayList<String> al1 = new ArrayList<String>();
-        ArrayList<String> al2 = new ArrayList<String>();
+        ArrayList<Keyword> al1 = new ArrayList<Keyword>();
+        ArrayList<Keyword> al2 = new ArrayList<Keyword>();
+        Keyword kw1 = new Keyword(1, "category", "fishing");
+        Keyword kw2 = new Keyword(2, "category", "accounting");
+        Keyword kw3 = new Keyword(3, "category", "pizza");
+        Keyword kw4 = new Keyword(4, "category", "test");
 
-        al1.add("pizza");
-        al1.add("fishing");
-        al1.add("accounting");
-        al2.add("pizza");
-        al2.add("fast");
+        al1.add(kw1);
+        al1.add(kw2);
+        al1.add(kw3);
+        al2.add(kw1);
+        al2.add(kw3);
+
         double db = calculateMatchingScore(al1, al2);
         System.out.println(db);
 
