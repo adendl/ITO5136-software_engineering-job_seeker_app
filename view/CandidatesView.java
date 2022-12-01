14df9942155624b13208fcf1bc1a4a8a932dc910
Heynus <ben.heynes@outlook.com>
2022-12-01 18:21:56 +0800

    Added table rendering to CandidatesView

--- view/CandidatesView.java
+++ view/CandidatesView.java
@@ -1,21 +1,39 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
+import model.DBConnection;
+import model.TableModelCreator;
 import view.UIView;
 import view.ViewHelper;
 
 public class CandidatesView implements UIView {
     private JPanel panelMain;
-    private JTable table1;
-    private JButton sortByButton;
+    private JTable jobSeekerTable;
+    private JButton searchButton;
     private JButton nextButton;
     private JButton previousButton;
     private JLabel headingText;
 
     public CandidatesView() {
+        ResultSet rs = DBConnection.queryDatabase(DBConnection.connectDb(), "select userID, firstName, LastName, email, phoneNumber, city, state from JobSeeker\njoin Location");
+        DefaultTableModel dft = TableModelCreator.resultSetToTableModel(rs);
+        ActionListener listener = e -> {
+            System.out.println("Button Clicked");
+        };
+
+        TableModelCreator.addActionColumn(dft, "Candidate Details", "More Information", listener);
+        jobSeekerTable.setModel(dft);
+        jobSeekerTable.setColumnSelectionAllowed(false);
+        jobSeekerTable.setRowSelectionAllowed(false);
+        JTableButtonRenderer renderer = new JTableButtonRenderer();
+        jobSeekerTable.getColumn(jobSeekerTable.getColumnName(jobSeekerTable.getColumnCount() - 1)).setCellRenderer(renderer);
+        jobSeekerTable.getColumn(jobSeekerTable.getColumnName(jobSeekerTable.getColumnCount() - 1)).setCellEditor(renderer);
+
         previousButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -40,7 +58,7 @@ public class CandidatesView implements UIView {
     }
 
     public JTable getTable1() {
-        return table1;
+        return jobSeekerTable;
     }
 
     public static void main(String[] args)
