4110e9f7f9fed364c1105e97a075067dafd17b23
Joel Paterno <joelpaterno1@gmail.com>
2022-12-02 16:08:35 +1100

    created and hooked up forgot password view

--- view/LoginView.java
+++ view/LoginView.java
@@ -32,6 +32,9 @@ public class LoginView implements UIView {
         createNewAccountButton.addActionListener((e) -> {
             controller.showCreateAccount();
         });
+        forgotPasswordButton.addActionListener((e -> {
+            controller.showForgotPassword();
+        }));
     }
 
     public JTextField getEmailText() {
