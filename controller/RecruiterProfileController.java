4f5b1defe24bf24d1d515d5a12e16ebc9be290df
Joel Paterno <joelpaterno1@gmail.com>
2022-12-02 15:28:06 +1100

    created and hooked up EditRecruiterProfileView

--- controller/RecruiterProfileController.java
+++ controller/RecruiterProfileController.java
@@ -1,5 +1,25 @@
 package controller;
 
-public class RecruiterProfileController {
+import model.User;
+import model.Recruiter;
+import view.EditRecuiterProfileView;
+import view.UIView;
 
+public class RecruiterProfileController {
+    NavigationController navigationController;
+    Recruiter user;
+    public RecruiterProfileController(NavigationController navigationController){
+        this.navigationController = navigationController;
+    }
+
+    public void showEditRecruiter(){
+        EditRecuiterProfileView editRecuiterProfileView = new EditRecuiterProfileView(this);
+        navigationController.pushView(editRecuiterProfileView);
+    }
+
+    public void updateProfile(String firstName, String lastName, String email, String phone, String company) {
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setCompany(company);
+    }
 }
