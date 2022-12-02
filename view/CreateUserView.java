dd1dc4599271b57b4b140c7a9b0827c5e5559e67
adel0025 <adel0025@student.monash.edu>
2022-12-02 15:50:33 +1100

    make app bootable

--- view/CreateUserView.java
+++ view/CreateUserView.java
@@ -3,6 +3,7 @@ package view;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 
+import controller.Validation;
 import view.UIView;
 import view.ViewHelper;
 import controller.LoginController;
@@ -30,6 +31,35 @@ public class CreateUserView implements UIView {
             createAccount();
         });
     }
+
+    public static boolean validateUser(String email, String password, String passwordConfirm, String firstName, String lastName)
+    {
+        if (!Validation.isEmail(email)) {
+            JOptionPane.showMessageDialog(null, "The email entered is not in the correct format of emailaddress@domain.com", "Invalid Email Address", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        if (!Validation.betweenLength(password, 8,255))
+        {
+            JOptionPane.showMessageDialog(null, "Passwords need to be between 8-255 characters long", "Password too short", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        if (!password.equals(passwordConfirm)) {
+            JOptionPane.showMessageDialog(null, "Passwords do not match!", "Password mismatch", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        if (!Validation.betweenLength(firstName, 2,255))
+        {
+            JOptionPane.showMessageDialog(null, "Names need to be between 2-255 characters long", "Name too short", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        if (!Validation.betweenLength(lastName, 2,255))
+        {
+            JOptionPane.showMessageDialog(null, "Names need to be between 2-255 characters long", "Name too short", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
     private void createAccount() {
         String email  = emailText.getText();
         String password = String.valueOf(enterPasswordText.getPassword());
@@ -38,12 +68,10 @@ public class CreateUserView implements UIView {
         String lastName = lastNameText.getText();
         int userType = selectUserTypeComboBox.getSelectedIndex();
 
-        if (!password.equals(passwordConfirm)) {
-            JOptionPane.showMessageDialog(null, "Passwords do not match!", "Password mismatch", JOptionPane.ERROR_MESSAGE);
-            return;
+        if (this.validateUser(email, password, passwordConfirm, firstName, lastName))
+        {
+            controller.doCreateAccount(firstName, lastName, email, password, userType);
         }
-
-        controller.doCreateAccount(firstName, lastName, email, password, userType);
     }
 
     public JPanel getMainPanel() {
