bb417d4a9bf1f5035d699288347aff00977debf2
Heynus <ben.heynes@outlook.com>
2022-12-03 18:27:57 +0800

    Added confirm and completion pages to message creation

--- controller/MailboxController.java
+++ controller/MailboxController.java
@@ -1,11 +1,10 @@
 package controller;
 
+import model.Job;
 import model.Mailbox;
 import model.Message;
 import model.User;
-import view.CreateMessageView;
-import view.MailboxView;
-import view.ReadMessageView;
+import view.*;
 
 import javax.swing.table.DefaultTableModel;
 import java.sql.SQLException;
@@ -56,21 +55,40 @@ public class MailboxController {
         navigationController.pushView(createMessageView);
     }
 
-    public void sendMessage(CreateMessageView view){
+    public void sendMessage(CreateMessageView view) {
         Message newMessage = new Message();
         newMessage.setContents(view.getMessageTextPane().getText());
         newMessage.setJobId(Integer.parseInt(view.getJobTextField().getText()));
         newMessage.setRecipientUserId(view.getToText().getText());
         newMessage.setSenderUserId(user.getUserId());
         newMessage.setSubject(view.getSubjectText().getText());
-
-        newMessage.createMessage();
-
-        this.showMailbox();
+        confirmSend(newMessage);
     }
 
+       public void confirmSend(Message message){
+        ConfirmPage confirmPage = new ConfirmPage(navigationController);
+        confirmPage.getYesButton().addActionListener(e -> {
+            message.createMessage();
+            CompletionPage completionPage = new CompletionPage(navigationController);
+            completionPage.getContinueButton().addActionListener(e1 -> {
+                this.showMailbox();
+            });
+            navigationController.pushView(completionPage);
+        });
+        confirmPage.getNoButton().addActionListener(e -> {
+            navigationController.popView();
+        });
+        navigationController.pushView(confirmPage);
+    }
 
+    public void sendApplyMessage(Job job) {
+        CreateMessageView createMessageView = new CreateMessageView(this);
+        createMessageView.getToText().setText(String.valueOf(job.getRecruiterId()));
+        createMessageView.getToText().setEnabled(false);
+        createMessageView.getSubjectText().setText("Application for " + job.getTitle() + " from " + user.getFirstName() + " " + user.getLastName());
+        createMessageView.getJobTextField().setText(String.valueOf(job.getJobId()));
+        createMessageView.getMessageTextPane().setText("Hello,\n I would like to apply for the role of " + job.getTitle() + ".\nKind Regards,\n" + user.getFirstName() + " " + user.getLastName());
 
-
-
+        sendMessage(createMessageView);
+    }
 }
